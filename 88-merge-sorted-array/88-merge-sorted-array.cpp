7eeec542229908f891568b247c78fa913a12bded
Bishop Bhaumik <60669056+Bishopbhaumik@users.noreply.github.com>
2022-03-18 18:01:57 +0530

    Time: 4 ms (53.39%), Space: 9.2 MB (30.34%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void merge(vector <int> &a , int m , vector <int> &b , int n)
+{
+    for(int i = 0 ; i < n ; i++)
+        a[i + m] = b[i];
+    sort(a.begin() , a.end());
+    return;
+}
+};
