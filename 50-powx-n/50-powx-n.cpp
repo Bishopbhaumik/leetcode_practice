81553e90b43afb111942f7ed8428f05deb47dc24
Bishop Bhaumik <60669056+Bishopbhaumik@users.noreply.github.com>
2022-03-24 19:29:51 +0530

    Time: 3 ms (37.11%), Space: 6 MB (18.36%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    double myPow2(double x,long long int n) {
-        
+    
+    double myPow(double x, long long int n) {
         if(n==0||x==1)
             return 1;
         
@@ -10,17 +10,13 @@ public:
         
         else if(n<0)
         {
-            return 1/myPow2(x,n/-1);
+            return 1/myPow(x,n/-1);
         }
        
         else if(n%2==0)
-            return myPow2(x*x,n/2);
+            return myPow(x*x,n/2);
         
         else 
-            return myPow2(x*x,n/2)*x;
-    }
-    
-    double myPow(double x, int n) {
-        return myPow2(x,n);
+            return myPow(x*x,n/2)*x;
     }
 };
