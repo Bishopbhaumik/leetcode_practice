d02e59464fb67ea6abbc562569205332a4d53c50
Bishop Bhaumik <60669056+Bishopbhaumik@users.noreply.github.com>
2022-04-10 20:45:14 +0530

    Time: 4 ms (71.34%), Space: 8.5 MB (28.60%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        
+        stack<int> result;
+        int point1,point2;
+
+        int sum = 0;
+        
+        for(auto i : ops)
+        {
+            if(i=="C")
+            {
+                result.pop();
+            }
+            
+            else if(i=="D")
+            {
+                result.push(result.top()*2);
+            }
+            
+            else if(i=="+")
+            {
+                point1 = result.top();
+                result.pop();
+                point2 = point1 + result.top();
+                result.push(point1);
+                result.push(point2);
+            }
+            
+            else
+            {
+                result.push(stoi(i));
+            }
+        }
+        
+        while(!result.empty())
+        {
+            sum+=result.top();
+            result.pop();
+        }
+        
+        return sum;
+    }
+};
